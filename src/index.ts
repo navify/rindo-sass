55d0fa4792dc936b8e151538b328ae9eab136914
NKDuy <kn145660@gmail.com>
2022-11-08 17:36:44 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,71 @@
+import { render } from 'sass';
+import * as d from './declarations';
+import { loadDiagnostic } from './diagnostics';
+import { createResultsId, getRenderOptions, usePlugin } from './util';
+
+
+export function sass(opts: d.PluginOptions = {}): d.Plugin {
+
+  return {
+    name: 'sass',
+    pluginType: 'css',
+    transform(sourceText, fileName, context) {
+      if (!usePlugin(fileName)) {
+        return null;
+      }
+      if (typeof sourceText !== 'string') {
+        return null;
+      }
+      const renderOpts = getRenderOptions(opts, sourceText, fileName, context);
+      const results: d.PluginTransformResults = {
+        id: createResultsId(fileName),
+        dependencies: []
+      };
+
+      if (sourceText.trim() === '') {
+        results.code = '';
+        return Promise.resolve(results);
+      }
+
+      return new Promise<d.PluginTransformResults>(resolve => {
+        try {
+          render(renderOpts, (err, sassResult) => {
+            if (err) {
+              loadDiagnostic(context, err, fileName);
+              results.code = `/**  sass error${err && err.message ? ': ' + err.message : ''}  **/`;
+              resolve(results);
+
+            } else {
+              results.dependencies = Array.from(sassResult.stats.includedFiles);
+              results.code = sassResult.css.toString();
+
+              // write this css content to memory only so it can be referenced
+              // later by other plugins (autoprefixer)
+              // but no need to actually write to disk
+              context.fs.writeFile(results.id, results.code, { inMemoryOnly: true }).then(() => {
+                resolve(results);
+              });
+            }
+          });
+
+        } catch (e) {
+          // who knows, just good to play it safe here
+          const diagnostic: d.Diagnostic = {
+            level: 'error',
+            type: 'css',
+            language: 'scss',
+            header: 'sass error',
+            relFilePath: null,
+            absFilePath: null,
+            messageText: e,
+            lines: []
+          };
+          context.diagnostics.push(diagnostic);
+
+          results.code = `/**  sass error${e && e.message ? ': ' + e.message : ''}  **/`;
+          resolve(results);
+        }
+      });
+    }
+  };
+}
---
+++ test/build.spec.ts
@@ -0,0 +1,92 @@
+import { sass } from '../dist';
+import { PluginCtx } from '../dist/declarations';
+
+import * as fs from 'fs';
+import * as path from 'path';
+
+
+describe('test build', () => {
+  let context: PluginCtx;
+
+  beforeEach(() => {
+    context = {
+      config: {
+        rootDir: '/Users/my/app/',
+        srcDir: '/Users/my/app/src/',
+      },
+      cache: null,
+      sys: {} as any,
+      fs: {
+        readFileSync(filePath: string) {
+          return fs.readFileSync(filePath, 'utf8');
+        },
+        writeFile() {
+          return Promise.resolve();
+        }
+      } as any,
+      diagnostics: []
+    };
+  })
+
+  it('transform', async () => {
+    const filePath = path.join(__dirname, 'fixtures', 'test-a.scss');
+    const sourceText = fs.readFileSync(filePath, 'utf8');
+    const s = sass();
+
+    const results = await s.transform(sourceText, filePath, context) as any;
+    expect(results.code).toContain('color: red');
+    expect(results.dependencies).toEqual([]);
+    expect(results.diagnostics).toEqual(undefined);
+  });
+
+  it('transform, import', async () => {
+    const filePath = path.join(__dirname, 'fixtures', 'test-b.scss');
+    const sourceText = fs.readFileSync(filePath, 'utf8');
+    const s = sass();
+
+    const results = await s.transform(sourceText, filePath, context) as any;
+    expect(results.code).toContain('color: red');
+    expect(results.dependencies).toEqual([
+      path.join(__dirname, 'fixtures', 'variables.scss')
+    ]);
+    expect(results.diagnostics).toEqual(undefined);
+  });
+
+  it('transform, error', async () => {
+    const filePath = path.join(__dirname, 'fixtures', 'test-c.scss');
+    const sourceText = fs.readFileSync(filePath, 'utf8');
+    const s = sass();
+
+    await s.transform(sourceText, filePath, context);
+    expect(context.diagnostics).toHaveLength(1);
+    expect(context.diagnostics[0].level).toEqual('error');
+    expect(context.diagnostics[0].language).toEqual('scss');
+    expect(context.diagnostics[0].lineNumber).toEqual(2);
+    expect(context.diagnostics[0].columnNumber).toEqual(23);
+    expect(context.diagnostics[0].lines.length).toEqual(3);
+
+    expect(context.diagnostics[0].lines[0].lineIndex).toEqual(0);
+    expect(context.diagnostics[0].lines[0].lineNumber).toEqual(1);
+    expect(context.diagnostics[0].lines[0].errorCharStart).toEqual(-1);
+    expect(context.diagnostics[0].lines[0].errorLength).toEqual(-1);
+    expect(context.diagnostics[0].lines[0].text).toEqual('body{color:blue}');
+
+    expect(context.diagnostics[0].lines[1].lineIndex).toEqual(1);
+    expect(context.diagnostics[0].lines[1].lineNumber).toEqual(2);
+    expect(context.diagnostics[0].lines[1].errorCharStart).toEqual(22);
+    expect(context.diagnostics[0].lines[1].errorLength).toEqual(1);
+    expect(context.diagnostics[0].lines[1].text).toEqual('   hello i am an error!');
+
+    expect(context.diagnostics[0].lines[2].lineIndex).toEqual(2);
+    expect(context.diagnostics[0].lines[2].lineNumber).toEqual(3);
+    expect(context.diagnostics[0].lines[2].errorCharStart).toEqual(-1);
+    expect(context.diagnostics[0].lines[2].errorLength).toEqual(-1);
+    expect(context.diagnostics[0].lines[2].text).toEqual('  div{color:green}');
+  });
+
+  it('name', async () => {
+    const s = sass();
+    expect(s.name).toBe('sass');
+  });
+
+});
